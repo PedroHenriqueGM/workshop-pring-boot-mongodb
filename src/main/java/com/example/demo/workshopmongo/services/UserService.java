f913e790b3488ed30c4fa2d9a7298c56604ac968
Pedro Magalhaes <phmagalhaes013@gmail.com>
2024-06-02 19:42:10 -0300

    Obtendo usuario por Id

--- src/main/java/com/example/demo/workshopmongo/services/UserService.java
+++ src/main/java/com/example/demo/workshopmongo/services/UserService.java
@@ -1,16 +1,17 @@
 package com.example.demo.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.workshopmongo.domain.User;
 import com.example.demo.workshopmongo.repositories.UserRepository;
+import com.example.demo.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
 	
 	@Autowired
 	private UserRepository repo;
@@ -18,5 +19,10 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 
 }
---
+++ src/main/java/com/example/demo/workshopmongo/services/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.demo.workshopmongo.services.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Nao encontrado", e.getMessage(), request.getRequestURI());
+		return (ResponseEntity<StandardError>) ResponseEntity.status(status).body(err);
+		
+	}
+}
