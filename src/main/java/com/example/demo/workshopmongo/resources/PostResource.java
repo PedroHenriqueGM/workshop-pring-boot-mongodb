8c130923d5dd3243f69ee76a478c7a407258d268
Pedro Magalhaes <phmagalhaes013@gmail.com>
2025-10-21 19:37:14 -0300

    Adiciona integração SonarCloud via GitHub Actions

--- src/main/java/com/example/demo/workshopmongo/resources/PostResource.java
+++ src/main/java/com/example/demo/workshopmongo/resources/PostResource.java
@@ -1,6 +1,7 @@
 package com.example.demo.workshopmongo.resources;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,20 @@ public class PostResource {
 		List<Post> list =service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value="/fullsearch")
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
 	
 
 }
